e5ba0284472ceb2a5e9db95301f13dbb340b40ee
Sebastian kurfer <sebastian.kurfer@kns-it.de>
2016-01-23 20:38:54 +0100

    Init commit, added base structure, added i18n files for main window, added libraries, added workflow basics

---
+++ QuickCodeStudio/QuickCodeStudio/MainWindow.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System.Activities.Core.Presentation;
+using System.Activities.Presentation;
+using System.Activities.Presentation.Toolbox;
+using System.Activities.Statements;
+using System.Windows.Controls;
+using System.Windows.Controls.Ribbon;
+
+namespace QuickCodeStudio
+{
+    /// <summary>
+    ///     Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : RibbonWindow
+    {
+
+        private WorkflowDesigner wd;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            RegisterMetadata();
+            AddDesigner();
+            AddToolBox();
+            AddPropertyInspector();
+        }
+
+
+        private void AddDesigner()
+        {
+            //Create an instance of WorkflowDesigner class.
+            this.wd = new WorkflowDesigner();
+
+            //Place the designer canvas in the middle column of the grid.
+            Grid.SetColumn(this.wd.View, 0);
+
+            //Load a new Sequence as default.
+            this.wd.Load(new Sequence());
+
+            //Add the designer canvas to the grid.
+            contentGrid.Children.Add(this.wd.View);
+        }
+
+        private void RegisterMetadata()
+        {
+            DesignerMetadata dm = new DesignerMetadata();
+            dm.Register();
+        }
+
+        private ToolboxControl GetToolboxControl()
+        {
+            // Create the ToolBoxControl.
+            ToolboxControl ctrl = new ToolboxControl();
+
+            // Create a category.
+            ToolboxCategory category = new ToolboxCategory("category1");
+
+            // Create Toolbox items.
+            ToolboxItemWrapper tool1 =
+                new ToolboxItemWrapper("System.Activities.Statements.Assign",
+                typeof(Assign).Assembly.FullName, null, "Assign");
+
+            ToolboxItemWrapper tool2 = new ToolboxItemWrapper("System.Activities.Statements.Sequence",
+                typeof(Sequence).Assembly.FullName, null, "Sequence");
+
+            // Add the Toolbox items to the category.
+            category.Add(tool1);
+            category.Add(tool2);
+
+            // Add the category to the ToolBox control.
+            ctrl.Categories.Add(category);
+            return ctrl;
+        }
+
+        private void AddToolBox()
+        {
+            ToolboxControl tc = GetToolboxControl();
+            Grid.SetColumn(tc, 0);
+            ToolPane.Children.Add(tc);
+        }
+
+        private void AddPropertyInspector()
+        {
+            Grid.SetColumn(wd.PropertyInspectorView, 2);
+            PropertyPane.Children.Add(wd.PropertyInspectorView);
+        }
+
+    }
+}
